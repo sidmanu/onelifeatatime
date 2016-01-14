95f6951dd5e6bccb7dff56701ce92d310110f71e
Siddharth Manu <siddharthmanu@gmail.com>
2016-01-14 17:01:23 +0000

    changing file settings to enable db operation

--- dialoguesforpeace/settings.py
+++ dialoguesforpeace/settings.py
@@ -82,3 +82,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = (
+        os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = '/home/ubuntu/onelifeatatime.in/dialoguesforpeace/static'
